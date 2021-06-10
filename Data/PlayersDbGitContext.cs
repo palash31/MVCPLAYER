6d4ae18dfe45193524916104419b969b54985548
palaaash.31@gmail.com <palaaash.31@gmail.com>
2021-06-10 15:10:57 +0530

    Second Commit

---
+++ Data/PlayersDbGitContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApp_Player_GIT.Data
+{
+    public class PlayersDbGitContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public PlayersDbGitContext() : base("name=PlayersDbGitContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<WebApp_Player_GIT.Models.Player> Players { get; set; }
+    }
+}
--- Models/Player.cs
+++ Models/Player.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace WebApp_Player_GIT.Models
-{
+{[Table("Player")]
     public class Player
     {
+        [Key]
+        public int Pid { get; set; }
+        public string Pname { get; set; }
+        public string Team { get; set; }
     }
 }
